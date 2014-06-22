743e93b442aabc4a47df9a5b29d219117f7d7c11
Pierre Dulac <dulacpier@gmail.com>
2014-06-22 21:01:02 +0200

    Refactor the plugin for more flexibility

--- src/revealing.js
+++ src/revealing.js
@@ -1,4 +1,4 @@
-/*
+/**
  * Revealing.js
  * 
  *
@@ -6,17 +6,50 @@
  * Licensed under the MIT license.
  */
 
-(function ($) {
+(function ($, window, document, undefined) {
+
+    var pluginName = "revealing",
+        defaults = {
+            /**
+             * Tokenizer method
+             *
+             * @param DOM Node element
+             * @return Array of wrapped nodes
+             */
+            tokenizer: function(node) {
+                if (node.nodeType === 3) {
+                    return node.nodeValue.split('').map(function(s) {
+                        return $('<span>'+s+'</span>');
+                    });
+                }
+
+                return null;
+            }
+        },
+        methods = {
+            show: function() {
+                this.removeClass('reset').addClass('reveal');
+            },
+            hide: function() {
+                this.removeClass('reset').removeClass('reveal');
+            },
+            reset: function() {
+                this.addClass('reset').removeClass('reveal');
+            }
+        };
 
     /**
-     * Recursively inject <span></span> elements to wrap each letters
+     * Recursively inject IN PLACE <span></span> elements to wrap each letters
      */
-    function injector(root) {
-        // text element
-        if (root.nodeType === 3) {
-            return root.nodeValue.split('').map(function(s) {
-                return $('<span>'+s+'</span>');
-            });
+    function injector(root, tokenizer) {
+        if (tokenizer === undefined) {
+            throw Error("You need to specify a tokenizer method");
+        }
+
+        // split elements
+        var tokens = tokenizer(root);
+        if (tokens !== null) {
+            return tokens;
         }
 
         var els = [],
@@ -33,39 +66,58 @@
 
         // recursively wrap text nodes
         nodes = els.reduce(function(mem, el) {
-            return mem.concat(injector(el));
+            return mem.concat(injector(el, tokenizer));
         }, []);
 
         // replace html content with new nodes
         return $(root).html('').append.apply($(root), nodes);
     }
 
-    var methods = {
-        show: function() {
-            this.removeClass('reset').addClass('reveal');
+    // plugin constructor
+    function Plugin(element, options) {
+        this.element = element;
+        this.settings = $.extend({}, defaults, options);
+        this._defaults = defaults;
+        this._name = pluginName;
+        this.init();
+    }
+
+    // Avoid Plugin.prototype conflicts
+    $.extend(Plugin.prototype, {
+        init: function () {
+            this.tokenize();
         },
-        hide: function() {
-            this.removeClass('reset').removeClass('reveal');
-        },
-        reset: function() {
-            this.addClass('reset').removeClass('reveal');
+        tokenize: function() {
+            injector(this.element, this.settings.tokenizer);
         }
-    };
+    });
 
-    $.fn.revealing = function(method) {
+    // expose the plugin
+    $.fn[pluginName] = function(method, options) {
+        if (typeof method === 'object') {
+            options = method;
+            method = undefined;
+        }
 
+        // build instances
+        this.each(function() {
+            var instance = $.data(this, "plugin_" + pluginName);
+            if (!instance) {
+                instance = new Plugin(this, options);
+                $.data(this, "plugin_" + pluginName, instance);
+            }
+        });
+
+        // custom method
         if (method !== undefined) {
             if (!methods.hasOwnProperty(method)) {
                 throw new Error("method " + method + " not supported by revealing.js");
             }
 
             methods[method].apply(this);
-            return this;
         }
 
-        return this.each(function() {
-            injector(this);
-        });
+        return this;
     };
 
-}(jQuery));
+}(jQuery, window, document));
