7c96fda3627a326eb85cc7048b3e34c61851e46d
Pierre Dulac <dulacpier@gmail.com>
2014-06-25 00:41:11 +0200

    Replace Grunt with gulp.js for code over configuration purposes

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+
+var jshint = require('gulp-jshint');
+var sass   = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+var rename = require('gulp-rename');
+var prefix = require('gulp-autoprefixer');
+var qunit  = require('gulp-qunit');
+
+gulp.task('lint', function() {
+    return gulp.src('js/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
+
+gulp.task('styles', function() {
+    return gulp.src('src/*.scss')
+        .pipe(sass())
+        .pipe(prefix('last 1 version', '> 1%', 'ie 8', 'ie 7'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('scripts', function() {
+    return gulp.src('src/*.js')
+        .pipe(concat('jquery.revealing.js'))
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('jquery.revealing.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('src/*.js',   ['lint', 'scripts']);
+    gulp.watch('src/*.scss', ['styles']);
+});
+
+gulp.task('qunit', function() {
+    return gulp.src('./test/test-runner.html')
+        .pipe(qunit());
+});
+
+gulp.task('build', ['scripts', 'styles']);
+gulp.task('test', ['lint', 'build', 'qunit']);
+gulp.task('default', ['lint', 'build', 'watch']);
