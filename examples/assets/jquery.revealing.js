7bd9a798fc504cdcec9cc67d53ae3d4bb06aa90c
Atala <alois.guillope@gmail.com>
2014-06-29 14:24:54 +0200

    feat: add a color option on plugin instanciation

--- examples/assets/jquery.revealing.js
+++ examples/assets/jquery.revealing.js
@@ -24,16 +24,17 @@
                 }
 
                 return null;
-            }
+            },
         },
         methods = {
-            show: function() {
+            show: function(args) {
+                this.css({"color": args["color"]});
                 this.removeClass('reset hide').addClass('reveal');
             },
-            hide: function() {
+            hide: function(args) {
                 this.removeClass('reset reveal').addClass('hide');
             },
-            reset: function() {
+            reset: function(args) {
                 this.removeClass('hide reveal').addClass('reset');
             }
         };
@@ -98,10 +99,17 @@
 
     // expose the plugin
     $.fn[pluginName] = function(method, options) {
+        var defaults = {
+                color: this.css("color")
+            },
+            options = options || {};
+
         if (typeof method === 'object') {
             options = method;
             method = undefined;
         }
+
+        options = $.extend(defaults, options);
 
         // build instances
         this.each(function() {
@@ -118,7 +126,7 @@
                 throw new Error("method " + method + " not supported by revealing.js");
             }
 
-            methods[method].apply(this);
+            methods[method].call(this, options);
         }
 
         return this;
